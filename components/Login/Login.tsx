068824bea4a123d73ab2481a783e4d1a03158a02
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-01-10 01:51:12 +0530

    importing client side auth

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { signIn, signOut, useSession } from "next-auth/react";
+import Button from "@mui/material/Button";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const { data: session } = useSession();
 
-export default Login
+  if (session) {
+    return (
+      <>
+        <Button variant={"contained"} color={"error"} onClick={() => signOut()}>
+          Sign out
+        </Button>
+      </>
+    );
+  }
+  return (
+    <>
+      <Button variant={"contained"} color={"success"} onClick={() => signIn()}>
+        Sign in
+      </Button>
+    </>
+  );
+};
+
+export default Login;
