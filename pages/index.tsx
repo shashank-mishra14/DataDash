6f89e58f73a439b39694101e86f3596369022202
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-01-10 16:00:47 +0530

    creating light and dark theme toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Dashboard from "@/pages/dashboard";
 import Login from "@/components/Login/Login";
 import { useSession } from "next-auth/react";
-import scss from "../components/Layout/Layout.module.scss";
+import scss from "./Home.module.scss";
 import React from "react";
 
 const Home: React.FC = () => {
