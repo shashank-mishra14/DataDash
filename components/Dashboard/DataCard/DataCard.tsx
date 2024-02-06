58170fd4bce59f2e0a3fe974414c36e4d8be3309
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-07 00:18:36 +0530

    changes in ui of datacard

--- components/Dashboard/DataCard/DataCard.tsx
+++ components/Dashboard/DataCard/DataCard.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Paper } from "@mui/material";
+import scss from "./DataCard.module.scss";
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
-const DataCard = () => {
+export type DataCardProps = {
+  title: string;
+  value: string;
+  description: string;
+};
+
+const DataCard = (props: DataCardProps) => {
+  const { title, value, description } = props;
   return (
-    <h1>DataCard</h1>
-  )
-}
+    <Paper className={scss.dataCard}>
+      <div className={scss.header}>
+        <Typography fontSize={"h6"} color={"lightslategrey"}>
+          {title}
+        </Typography>
+        <Tooltip
+          title={
+            <Typography
+              fontSize={16}
+            >{`${description} which is ${value}`}</Typography>
+          }
+        >
+          <IconButton>
+            <InfoOutlinedIcon />
+          </IconButton>
+        </Tooltip>
+      </div>
+      <Typography fontSize={"h4"}>{value}</Typography>
+    </Paper>
+  );
+};
 
-export default DataCard
+export default DataCard;
